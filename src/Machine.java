08b6c55c94b89ef083a531ce62ed4a2da906a2b3
Daria <darvel73@yahoo.com>
2024-11-29 21:12:47 +0800

    Coffeemachine2

--- src/Machine.java
+++ src/Machine.java
@@ -1,34 +1,42 @@
 public class Machine {
-    double esp = 8;
-    int milk = 7;
-    int iCream = 3;
-    int syrup = 4;
-    int juice = 5;
+    double esp;
+    int milk;
+    int iCream;
+    int syrup;
+    int juice;
 
-    public void espresso() {
-        System.out.println((esp >= 8) ? "Готовится эспрессо" : "Недостаточно ингридиентов");
+    public Machine(double esp, int milk,int iCream,int syrup,int juice) {
+        this.esp = esp;
+        this.milk = milk;
+        this.iCream = iCream;
+        this.syrup = syrup;
+        this.juice = juice;
     }
-    public void milk() {
-        System.out.println((milk >= 8) ? "Готовится молоко" : "Недостаточно ингридиентов");
+        public void espresso () {
+            System.out.println((esp >= 8) ? "Готовится эспрессо" : "Недостаточно ингридиентов");
+        }
+        public void milk () {
+            System.out.println((milk >= 8) ? "Готовится молоко" : "Недостаточно ингридиентов");
+        }
+        public void iceCream () {
+            System.out.println((iCream >= 8) ? "Готовится мороженное" : "Недостаточно ингридиентов");
+        }
+        public void syrup () {
+            System.out.println((syrup >= 8) ? "Готовится сироп" : "Недостаточно ингридиентов");
+        }
+        public void juice () {
+            System.out.println((juice >= 8) ? "Готовится сок" : "Недостаточно ингридиентов");
+        }
+        public void flatWhite () {
+            System.out.println((esp >= 8 * 0.4 && milk >= 8 * 0.6) ? "Готовится Флэт-Уйат" : "Недостаточно ингридиентов");
+        }
+        public void latte () {
+            System.out.println((esp >= 8 * 0.15 && milk >= 8 * 0.85) ? "Готовится Латте" : "Недостаточно ингридиентов");
+        }
+        public void smoothie () {
+            System.out.println((syrup >= 8 * 0.1 && iCream >= 8 * 0.5 && esp >= 8 * 0.35) ?
+                    "Готовится смузи" : "Недостаточно ингридиентов");
+        }
     }
-    public void iceCream() {
-        System.out.println((iCream >= 8) ? "Готовится мороженное" : "Недостаточно ингридиентов");
-    }
-    public void syrup() {
-        System.out.println((syrup >= 8) ? "Готовится сироп" : "Недостаточно ингридиентов");
-    }
-    public void juice() {
-        System.out.println((juice >= 8) ? "Готовится сок" : "Недостаточно ингридиентов");
-    }
-    public void flatWhite() {
-        System.out.println((esp >= 8 * 0.4 && milk >= 8 * 0.6) ? "Готовится Флэт-Уйат" : "Недостаточно ингридиентов");
-    }
-    public void latte() {
-        System.out.println((esp >= 8 * 0.15 && milk >= 8 * 0.85) ? "Готовится Латте" : "Недостаточно ингридиентов");
-    }
-    public void smoothie() {
-        System.out.println((syrup >= 8 * 0.1 && iCream >= 8 * 0.5 && esp >= 8 * 0.35) ?
-                "Готовится смузи" : "Недостаточно ингридиентов");
-    }
-}
+
 
--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Machine coffeeMachine = new Machine();
+        Machine coffeeMachine = new Machine (8,7,3,4,5);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите номер напитка: \n 1 - Эспрессо \n 2 - Молоко\n 3 - Мороженное\n 4 - Сироп" +
                 "\n 5 - Сок\n 6 - Флэт-Уайт\n 7 - Латте\n 8 - Смузи\n Чтобы выключить, введите 0");
-        int instruction = scanner.nextInt();
-        switch (instruction) {
-            case 1:
-                coffeeMachine.espresso();
+        while(true) {
+            int instruction = scanner.nextInt();
+            switch (instruction) {
+                case 1:
+                    coffeeMachine.espresso();
+                    break;
+                case 2:
+                    coffeeMachine.milk();
+                    break;
+                case 3:
+                    coffeeMachine.iceCream();
+                    break;
+                case 4:
+                    coffeeMachine.syrup();
+                    break;
+                case 5:
+                    coffeeMachine.juice();
+                    break;
+                case 6:
+                    coffeeMachine.flatWhite();
+                    break;
+                case 7:
+                    coffeeMachine.latte();
+                    break;
+                case 8:
+                    coffeeMachine.smoothie();
+                    break;
+            }
+            if (instruction==0){
                 break;
-            case 2:
-                coffeeMachine.milk();
-                break;
-            case 3:
-                coffeeMachine.iceCream();
-                break;
-            case 4:
-                coffeeMachine.syrup();
-                break;
-            case 5:
-                coffeeMachine.juice();
-                break;
-            case 6:
-                coffeeMachine.flatWhite();
-                break;
-            case 7:
-                coffeeMachine.latte();
-                break;
-            case 8:
-                coffeeMachine.smoothie();
-                break;
-            case 0:
-                break;
+            }
         }
     }
 }
